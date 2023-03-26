8e149d4c8b2aef4c14bec671215936d693fcc0f5
Niko <113851733+nikosobi@users.noreply.github.com>
2023-03-26 15:41:30 +0200

    My CarFix project uploaded to GitHub

---
+++ initialData/providers.js
@@ -0,0 +1,9 @@
+module.exports = [
+    {
+        name: "Prov",
+        bio: "provprovprovprovprovprov",
+        location: "Durres",
+        image_url: "http://localhost:5001/static/download.jpeg",
+        id: 20653260
+    }
+]
